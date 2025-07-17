5f7caee05a39fcd7d1e8984106d174ec4640fcfc
SoloGudLyf <solomonteiko0@gmail.com>
2025-07-17 05:22:46 +0000

    Added function to display book to page as cards

--- script.js
+++ script.js
@@ -21,4 +21,19 @@ function Book(
 let book1 = new Book("My book", "Myself", 34, true);
 myLibrary.push(book1);
 
-console.log(myLibrary);
+function displayBookToPage() {
+  for (let book of myLibrary) {
+    let parent = document.querySelector(".gridBox");
+    let card = document.createElement("article");
+    card.classList.add("book");
+    let bookInfo = `Book name : <span class="book-info¨>${book.name}</span> <br> 
+    Author : <span class="book-info">${book.author}</span> <br>
+    Number of Pages : <span class="book-info">${book.numberOfPages}</span> <br>
+    <label for="readBook">Read </label><input type="checkbox" name="readBook" id="readBook"> <br>
+    Book ID : <span class="book-info">${book.Id}</span>`;
+    card.innerHTML = bookInfo;
+    parent.appendChild(card);
+  }
+}
+
+displayBookToPage();
