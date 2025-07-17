22af480037584f30135c14a797e1e976bc661f01
SoloGudLyf <solomonteiko0@gmail.com>
2025-07-17 06:25:24 +0000

    feat(HTML):Add dialog form for creating new books
    feat(JS): Open dialog on button click
              Handle form submission to create book
              Add new book to myLibrary and display on page

--- script.js
+++ script.js
@@ -22,18 +22,50 @@ let book1 = new Book("My book", "Myself", 34, true);
 myLibrary.push(book1);
 
 function displayBookToPage() {
+  let parent = document.querySelector(".gridBox");
+  parent.innerHTML = "";
   for (let book of myLibrary) {
-    let parent = document.querySelector(".gridBox");
     let card = document.createElement("article");
     card.classList.add("book");
-    let bookInfo = `Book name : <span class="book-info¨>${book.name}</span> <br> 
+    let bookInfo = `Book name : <span class="book-info">${
+      book.name
+    }</span> <br> 
     Author : <span class="book-info">${book.author}</span> <br>
     Number of Pages : <span class="book-info">${book.numberOfPages}</span> <br>
-    <label for="readBook">Read </label><input type="checkbox" name="readBook" id="readBook"> <br>
+    <label for="readBook">Read </label><input type="checkbox" name="readBook" id="readBook-${
+      book.Id
+    }" ${book.readBook ? "checked" : ""}> <br>
     Book ID : <span class="book-info">${book.Id}</span>`;
     card.innerHTML = bookInfo;
     parent.appendChild(card);
   }
 }
 
+// Show Dialog and get form data
+
+let form = document.querySelector("form");
+let dialog = document.querySelector("dialog");
+let openDialog = document.querySelector(".openDialog");
+openDialog.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+function getFormInput() {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let formData = new FormData(form);
+    let bookName = formData.get("bookName");
+    let author = formData.get("author");
+    let numberOfPages = formData.get("numberOfPages");
+    let readBook = formData.get("readBook");
+    readBook == null ? (readBook = false) : (readBook = true);
+    let newBook = new Book(bookName, author, numberOfPages, readBook);
+    myLibrary.push(newBook);
+
+    dialog.close();
+    displayBookToPage();
+  });
+}
+
+getFormInput();
 displayBookToPage();
